b4e01c66b3067756ea404988c57636acc81bc6e3
Brent <brent@hawkeyes.org>
2026-03-30 23:35:53 -0500

    test: use temporary input folder in CLI argument parsing test

--- src/CmdrsChronicle.Tests/CliArgumentParsingTests.cs
+++ src/CmdrsChronicle.Tests/CliArgumentParsingTests.cs
@@ -1,9 +1,8 @@
 using System.CommandLine;
-using System.CommandLine;
+using System;
 using Xunit;
 using System.Threading.Tasks;
 using System.CommandLine.Invocation;
-using System.CommandLine.Testing;
 
 namespace CmdrsChronicle.Cli.Tests
 {
@@ -13,9 +12,18 @@ namespace CmdrsChronicle.Cli.Tests
         public async Task HelpOption_PrintsHelp()
         {
             var rootCommand = Program.BuildRootCommand();
-            var console = new TestConsole();
-            var result = await rootCommand.InvokeAsync("--help", console);
-            var output = console.Out.ToString();
+            var sw = new System.IO.StringWriter();
+            var originalOut = Console.Out;
+            Console.SetOut(sw);
+            try
+            {
+                var result = rootCommand.Invoke("--help");
+            }
+            finally
+            {
+                Console.SetOut(originalOut);
+            }
+            var output = sw.ToString();
             Assert.Contains("CmdrsChronicle CLI", output);
             Assert.Contains("--start", output);
             Assert.Contains("--end", output);
@@ -28,10 +36,15 @@ namespace CmdrsChronicle.Cli.Tests
         public async Task ArgumentParsing_ParsesAllOptions()
         {
             var rootCommand = Program.BuildRootCommand();
-            var console = new TestConsole();
+            var sw = new System.IO.StringWriter();
+            var originalOut = Console.Out;
+            Console.SetOut(sw);
+            // Create and use a temporary input directory so the test doesn't require a fixed path
+            var tempInput = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "CmdrsChronicleTestInput_" + System.Guid.NewGuid().ToString("N"));
+            System.IO.Directory.CreateDirectory(tempInput);
             var args = new[]
             {
-                "--input", "C:/logs",
+                "--input", tempInput,
                 "--output", "report.html",
                 "--start", "2026-03-01",
                 "--end", "2026-03-07",
@@ -40,9 +53,17 @@ namespace CmdrsChronicle.Cli.Tests
                 "--style", "colorful",
                 "--max-parallelism", "4"
             };
-            var result = await rootCommand.InvokeAsync(args, console);
-            // No exception = success
-            Assert.Equal(0, result);
+            try
+            {
+                var result = rootCommand.Invoke(args);
+                // No exception = success
+                Assert.Equal(0, result);
+            }
+            finally
+            {
+                Console.SetOut(originalOut);
+                try { System.IO.Directory.Delete(tempInput, true); } catch { }
+            }
         }
     }
 }
